2a005e6bdcdfa3c28e3bd0105a7e1d994a9293a8
Dorin Cretu <dorin.cretu@pm.me>
2021-07-25 08:42:49 +0200

    0.4

---
+++ graphql/resolvers/PostResolver.ts
@@ -0,0 +1,44 @@
+import { CreatePostInput } from "../inputs/CreatePostInput";
+import { UpdatePostInput } from "../inputs/UpdatePostInput";
+import { Post } from "../models/Post";
+import { Resolver, Query, Mutation, Arg } from "type-graphql";
+
+@Resolver(Post)
+export class PostResolver {
+    @Query(() => [Post])
+    posts() {
+        return Post.find();
+    }
+
+    @Query(() => Post)
+    async post(@Arg("id") id: string) {
+        let post = await Post.findOne({ where: { id } });
+
+        if (!post) throw new Error("Post not found!");
+        return post
+    }
+
+    @Mutation(() => Post)
+    async createPost(@Arg("data") data: CreatePostInput) {
+        const post = Post.create(data);
+        await post.save();
+        return post;
+    }
+
+    @Mutation(() => Post)
+    async updatePost(@Arg("id") id: string, @Arg("data") data: UpdatePostInput) {
+        const post = await Post.findOne({ where: { id } });
+        if (!post) throw new Error("Post not found!");
+        Object.assign(post, data);
+        await post.save();
+        return post;
+    }
+
+    @Mutation(() => Boolean)
+    async deletePost(@Arg("id") id: string) {
+        const post = await Post.findOne({ where: { id } });
+        if (!post) throw new Error("Post not found!");
+        await post.remove();
+        return true;
+    }
+}
