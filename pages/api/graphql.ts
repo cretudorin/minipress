0d9411fe69ae9b3d48e19e6c8d74dfaee9730dc7
Dorin Cretu <dorin.cretu@pm.me>
2021-08-05 22:13:09 +0200

    0.6 working graphql

---
+++ pages/api/graphql.ts
@@ -0,0 +1,40 @@
+// Polyfill needed for type-graphql
+import "reflect-metadata";
+
+import { ApolloServer } from "apollo-server-micro";
+import { buildSchema } from "type-graphql";
+import { IncomingMessage, ServerResponse } from "http";
+
+import { User, UserResolver } from "./lib/features/user";
+
+export interface Context {
+    user: User;
+}
+
+// register 3rd party IOC container
+// TypeORM.useContainer(Container);
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
+let apolloServerHandler: (req: any, res: any) => Promise<void>;
+
+/**
+ * Create the Apollo Server.
+ */
+const getApolloServerHandler = async () => {
+    if (!apolloServerHandler) {
+        const schema = await buildSchema({
+            resolvers: [UserResolver],
+        });
+        apolloServerHandler = new ApolloServer({ schema }).createHandler({
+            path: "/api/graphql",
+        })
+    }
+    return apolloServerHandler;
+};
+
+export default async (req: IncomingMessage, res: ServerResponse) =>  (await getApolloServerHandler())(req, res);
---
+++ pages/api/lib/db/db.ts
@@ -0,0 +1,28 @@
+import * as TypeORM from "typeorm";
+
+import { User } from "../features/user";
+
+let databaseConnection: TypeORM.Connection;
+
+export const createConnection = async (): Promise<TypeORM.Connection> => {
+    if (databaseConnection) {
+        return databaseConnection;
+    }
+    // create TypeORM connection
+    try {
+        databaseConnection = await TypeORM.createConnection({
+            type: "sqlite",
+            database: process.env.DATABASE_URL!!
+        });
+    } catch (e) {
+        console.error(
+            "Could not create a connection with the database, check settings!",
+            e
+        );
+        throw e;
+    }
+    if (!databaseConnection) {
+        throw new Error("database connection still does not exist!");
+    }
+    return databaseConnection;
+};
